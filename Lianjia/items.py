3c9ee93db65c975e1e9408ce1b0b1b4aff048222
Ray-Xu <xurui617@hotmail.com>
2019-12-09 22:59:12 +0800

    Add collected Info
    20191209 23:00

--- Lianjia/items.py
+++ Lianjia/items.py
@@ -11,6 +11,33 @@ import scrapy
 class LianjiaItem(scrapy.Item):
     # define the fields for your item here like:
     # name = scrapy.Field()
-
+    链接 = scrapy.Field()
+    小区名称 = scrapy.Field()
+    所在区域 = scrapy.Field()
+    地铁站 = scrapy.Field()
+    总价 = scrapy.Field()
+    单价 = scrapy.Field()
+    建筑时间 = scrapy.Field()
+    # base info
+    房屋户型 = scrapy.Field()
+    所在楼层 = scrapy.Field()
+    建筑面积 = scrapy.Field()
+    户型结构 = scrapy.Field()
+    套内面积 = scrapy.Field()
+    建筑类型 = scrapy.Field()
+    房屋朝向 = scrapy.Field()
+    建筑结构 = scrapy.Field()
+    装修情况 = scrapy.Field()
+    梯户比例 = scrapy.Field()
+    配备电梯 = scrapy.Field()
+    产权年限 = scrapy.Field()
+    # trade info
+    挂牌时间 = scrapy.Field()
+    交易权属 = scrapy.Field()
+    上次交易 = scrapy.Field()
+    房屋用途 = scrapy.Field()
+    房屋年限 = scrapy.Field()
+    产权所属 = scrapy.Field()
+    抵押信息 = scrapy.Field()
 
     pass
--- Lianjia/spiders/Lianjia.py
+++ Lianjia/spiders/Lianjia.py
@@ -16,17 +16,28 @@ class MySpider(scrapy.Spider):
         district = response.xpath('//div[@data-role="ershoufang"]//a//@href').extract()
         for url in district:
             district_url = self.base_url + url
-            print("=============", district_url)
+            # print("=============", district_url)
             yield Request(district_url, self.get_urls)
 
     def get_urls(self, response):
-        print("*********************")
+        # print("*********************")
         urls = response.xpath('//ul[@class="sellListContent"]//li//div[@class="title"]//a//@href').extract()
         for url in urls:
             yield Request(url, self.get_info)
 
     def get_info(self, response):
-        print("++++++++++++++++")
+        # print("++++++++++++++++")
+        item = LianjiaItem()
+
+        item.链接 = response.url
+        item.小区名称 = response.xpath('//div[@class="communityName"]//text()').extract()[1]
+        item.所在区域 = scrapy.Field()
+        item.地铁站 = scrapy.Field()
+        item.总价 = response.xpath('//span[@class="total"]//text()').extract()
+        item.单价 = response.xpath('//span[@class="unitPriceValue"]//text()').extract()[0]
+        item.建筑时间 = response.xpath('//div[@class="area"]//div[@class="subInfo"]//text()').extract()[0].split('/')[0]
+
+
 
         pass
 
